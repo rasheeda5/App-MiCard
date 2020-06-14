feea3305845ed707bb25b7e7ac3a626dbdd58402
Rasheeda Abbas <rasheedaabbas@gmail.com>
2020-06-14 21:44:08 +0500

    basic layout + picture added

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Column(
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50.0,
+                //backgroundColor: Colors.black,
+                backgroundImage: AssetImage('images/profilepic.png'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
